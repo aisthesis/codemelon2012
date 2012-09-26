a823d64abcab2a0bdb0e94136899b08ad97e7bd7
Marshall Farrier <info@marshallfarrier.com>
2012-09-26 08:30:57 -0700

    PHP to include sidebar CSS only when needed.

--- includes/common.php
+++ includes/common.php
@@ -11,4 +11,14 @@ namespace common;
 function javaScriptFile($page) {
     echo '"scripts/content/' . $page . '.js"';
 }
+
+function includeSidebarCss($page) {
+    // not included on pages: links
+    // take advantage of PHP's fallthrough logic in switch
+    switch ($page) {
+        case "home":
+        case "blah":
+            echo '<script type="text/javascript" src="scripts/common/content-sidebar.js"></script>';
+    }
+}
 ?>
