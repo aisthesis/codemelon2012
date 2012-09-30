79a3a857377686989848f44f0d3a5d1bf8a4adaa
Marshall Farrier <info@marshallfarrier.com>
2012-09-29 23:20:11 -0700

    Attempt to correct error occurring only on remote server

--- includes/content.php
+++ includes/content.php
@@ -1,5 +1,5 @@
 <?php
-namespace content {    
+namespace content;    
 /*
  * @file includes/content.php
  * @site codeMelon2012
@@ -8,33 +8,33 @@ namespace content {
  * @description PHP functions for linking specific page content
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
-    function styling($page) {
-        echo '<link href="styles/content/' . $page . '.css" rel="stylesheet" type="text/css" />';
-    }
-    
-    function includeContentHeader($page) {
-        // not included on pages: links
-        // take advantage of PHP's fallthrough logic in switch
-        switch ($page) {
-            case "home":
-            case "other pages using content header":
-                echo '<div id="header"><table><tr>';
-                echo '<td><div class="outer"><div class="image-wrapper">';
-                echo '<img width="240" height="200" src="images/RoboticArm.jpg" />';
-                echo '</div></div></td>';
-                echo '<td><div class="outer"><div class="image-wrapper">';
-                echo '<img width="240" height="200" src="images/JeffRowleyJaws2012.jpg" />';
-                echo '</div></div></td>';
-                echo '<td><div class="outer"><div class="image-wrapper">';
-                echo '<iframe width="240" height="200" src="http://www.youtube.com/embed/QmWD76jwjbQ" frameborder="0"></iframe>';
-                echo '</div></div></td>';
-                echo '</tr></table></div>';
-                break;
-            case "links":
-                echo '<div id="header">';
-                echo '<h2>Additional Resources</h2>';
-                echo '</div>';
-        }
+function styling($page) {
+    echo '<link href="styles/content/' . $page . '.css" rel="stylesheet" type="text/css" />';
+}
+
+function includeContentHeader($page) {
+    // not included on pages: links
+    // take advantage of PHP's fallthrough logic in switch
+    switch ($page) {
+        case "home":
+        case "other pages using content header":
+            echo '<div id="header"><table><tr>';
+            echo '<td><div class="outer"><div class="image-wrapper">';
+            echo '<img width="240" height="200" src="images/RoboticArm.jpg" />';
+            echo '</div></div></td>';
+            echo '<td><div class="outer"><div class="image-wrapper">';
+            echo '<img width="240" height="200" src="images/JeffRowleyJaws2012.jpg" />';
+            echo '</div></div></td>';
+            echo '<td><div class="outer"><div class="image-wrapper">';
+            echo '<iframe width="240" height="200" src="http://www.youtube.com/embed/QmWD76jwjbQ" frameborder="0"></iframe>';
+            echo '</div></div></td>';
+            echo '</tr></table></div>';
+            break;
+        case "links":
+            echo '<div id="header">';
+            echo '<h2>Additional Resources</h2>';
+            echo '</div>';
     }
 }
+
 ?>
---
+++ phpinfo.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * @file phpinfo.php
+ * @site codeMelon2012
+ * @author Marshall Farrier
+ * @date 09/29/2012
+ * @description provide info on PHP version
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+phpinfo();
+?>
