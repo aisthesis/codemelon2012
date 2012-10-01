a944017b06fc6746dcc229201de726d286dfbe15
Marshall Farrier <info@marshallfarrier.com>
2012-09-30 22:24:14 -0700

    Corrected problems with variable declaration

--- scripts/common/content-sidebar.js
+++ scripts/common/content-sidebar.js
@@ -8,19 +8,19 @@
  */
 
 function contentSidebarMain() {
-    duration = 1500;
+    var duration = 1500;
     $('#content-sidebar h4.channels').click(function() {        
         $('#main-content section').animate({'opacity' : 1}, duration);
     });
-    highlightChannel('apps');
-    highlightChannel('programming');
-    highlightChannel('robotics');
-    highlightChannel('site');
-    highlightChannel('tutorials');
+    highlightChannel('apps', duration);
+    highlightChannel('programming', duration);
+    highlightChannel('robotics', duration);
+    highlightChannel('site', duration);
+    highlightChannel('tutorials', duration);
     highlightChannel('web');
 }
 
-function highlightChannel(channel) {
+function highlightChannel(channel, duration) {
     $('#content-sidebar .' + channel + ' a').click(function() {
         $('#main-content section').animate({'opacity' : 0.1}, duration);
         $('#main-content section.' + channel).animate({'opacity' : 1}, duration);
