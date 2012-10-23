9ee4346bf2b7fadf7e732197561b8a48a5dda729
Marshall Farrier <info@marshallfarrier.com>
2012-10-22 19:47:56 -0700

    Toggle menu arrow

--- scripts/content/articles.js
+++ scripts/content/articles.js
@@ -6,6 +6,33 @@
  * @description JavaScript for codemelon2012 articles
  */
 $(document).ready(function() {
+    articleMenu();
     codeMelonMain('articles');
 });
 
+function articleMenu() {
+    $('#menu-arrow').click(function() {
+        if ($(this).hasClass('down')) { openArticleMenu(); }
+        else { closeArticleMenu(); }
+    });
+}
+
+function openArticleMenu() {
+    $('#menu-arrow').stop().animate(
+        {opacity: 0},
+        500,
+        function(){
+            $(this).removeClass('down').addClass('up').animate({opacity: 1}, 500);
+        }
+    );   
+}
+
+function closeArticleMenu() {  
+    $('#menu-arrow').stop().animate(
+        {opacity: 0},
+        500,
+        function(){
+            $(this).removeClass('up').addClass('down').animate({opacity: 1}, 500);
+        }
+    );
+}
